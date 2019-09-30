5ebcd90eef3412dc889235dbaebe64158f7433ac
Dane Finlay <Danesprite@posteo.net>
2019-09-30 15:20:34 +1000

    Add FileActivity and update FileUriHelper.kt
    - FileActivity does most of what MainActivity used to do with
      files.
    - Update each extension function in FileUriHelper.kt and remove
      a few things that aren't used anymore.

---
+++ app/src/main/java/com/danefinlay/ttsutil/FileActivity.kt
@@ -0,0 +1,225 @@
+package com.danefinlay.ttsutil
+
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
+import com.danefinlay.androidutil.FileChooser
+import org.jetbrains.anko.*
+import java.io.File
+
+
+private const val CHOSEN_FILE_URI_KEY = "$APP_NAME.CHOSEN_FILE_URI_KEY"
+private const val CHOSEN_FILE_NAME_KEY = "$APP_NAME.CHOSEN_FILE_NAME_KEY"
+
+class FileActivity : MyAppCompatActivity(), FileChooser {
+
+    override var chooseFileAction = Intent.ACTION_OPEN_DOCUMENT
+    override var chooseFileCategory = Intent.CATEGORY_OPENABLE
+    override var chooseFileMimeType = "text/*"
+    override fun getActivity() = this
+
+    private var fileToRead: Uri? = null
+
+    private val chooseFileButton: Button
+        get() = find(R.id.choose_file_button)
+
+    private val chosenFilename: TextView
+        get() = find(R.id.chosen_filename)
+
+    private val readFileButton: Button
+        get() = find(R.id.read_file_button)
+
+    private val stopSpeakingButton: Button
+        get() = find(R.id.stop_speaking_button)
+
+    private val writeToFileButton: Button
+        get() = find(R.id.write_to_file_button)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        if (savedInstanceState == null) {
+            // Get the last chosen file's URI and display name from shared
+            // preferences.
+            val prefs = getSharedPreferences(packageName, MODE_PRIVATE)
+            val uri = Uri.parse(prefs.getString(CHOSEN_FILE_URI_KEY, ""))
+            setFileUriAndDisplayName(uri)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putParcelable(CHOSEN_FILE_URI_KEY, fileToRead)
+        outState?.putCharSequence(CHOSEN_FILE_NAME_KEY, chosenFilename.text)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        savedInstanceState?.apply {
+            fileToRead = getParcelable(CHOSEN_FILE_URI_KEY)
+            chosenFilename.text = getCharSequence(CHOSEN_FILE_NAME_KEY)
+        }
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when ( item.itemId ) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun buildInvalidFileAlertDialog(): AlertDialogBuilder {
+        return AlertDialogBuilder(ctx).apply {
+            title(getString(R.string.invalid_file_dialog_title))
+            message(getString(R.string.invalid_file_dialog_message))
+            positiveButton(getString(R.string.alert_positive_message)) {
+                showFileChooser()
+            }
+            negativeButton(R.string.alert_negative_message1)
+        }
+    }
+
+    private fun onClickReadFile() {
+        val validFile: Boolean? = fileToRead?.validFilePath(ctx)
+        if (validFile == true) {
+            fileToRead?.getContent(ctx)?.reader()?.forEachLine {
+                speaker?.speak(it)
+            }
+        } else if (validFile == false) buildInvalidFileAlertDialog().show()
+    }
+
+    private fun onClickWriteFile() {
+        val uri = fileToRead
+
+        // Build and display an appropriate alert dialog.
+        val uriIsValid = uri != null && uri.validFilePath(this)
+        val msg = getString(R.string.write_to_file_alert_message)
+        if (!uriIsValid) {
+            buildInvalidFileAlertDialog().show()
+            return
+        }
+
+        AlertDialogBuilder(this).apply {
+            title(getString(R.string.file_activity_description2))
+            message("$msg \"${uri?.getDisplayName(ctx)}\"")
+            positiveButton(getString(R.string.alert_positive_message)) {
+                // Ask the user for write permission if necessary.
+                verifyStoragePermissions()
+
+                // Check again before continuing.
+                if (uri == null || !uri.validFilePath(ctx)) {
+                    runOnUiThread {buildInvalidFileAlertDialog().show()}
+                    return@positiveButton
+                }
+
+                val content = uri.getContent(ctx)?.reader()?.readText()
+                if (content.isNullOrBlank()) {
+                    // Nothing to read.
+                    return@positiveButton
+                }
+
+                val dir = Environment.getExternalStorageDirectory()
+                val file = File(dir, "speech.mp4")
+                val successDialogBuilder = AlertDialogBuilder(ctx).apply {
+                    title(R.string.file_activity_description2)
+                    message(R.string.write_to_file_alert_message_success)
+                    positiveButton(R.string.alert_positive_message) {}
+                    neutralButton(R.string.open_file_message) {
+                        // Open the wave file.
+                        // val intent = Intent()
+                        // intent.action = ACTION_OPEN_DOCUMENT
+                        // intent.putExtra()
+                        // startActivity()
+                    }
+                }
+                speaker?.synthesizeToFile(content, file) {
+                    if (it is Speaker.UtteranceProgress.Done)
+                        runOnUiThread {successDialogBuilder.show()}
+                }
+            }
+            negativeButton(R.string.alert_negative_message2)
+
+            // Show the dialog.
+            show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        chooseFileButton.onClick { showFileChooser() }
+        readFileButton.onClick { onClickReadFile() }
+        writeToFileButton.onClick { onClickWriteFile() }
+        stopSpeakingButton.onClick { speaker?.stopSpeech() }
+    }
+
+    override fun onFileChosen(uri: Uri?) {
+        setFileUriAndDisplayName(uri)
+
+        // Set the shared preference values asynchronously.
+        doAsync {
+            val prefs = getSharedPreferences(packageName, MODE_PRIVATE)
+            prefs.edit()
+                    .putString(CHOSEN_FILE_URI_KEY, uri?.toString() ?: "")
+                    .putString(CHOSEN_FILE_NAME_KEY,
+                            uri?.getDisplayName(this@FileActivity) ?: "")
+                    .apply()
+        }
+    }
+
+    private fun setFileUriAndDisplayName(uri: Uri?) {
+        // Set chosenFilename.text to the file's display name if possible.
+        val noFileChosen = getString(R.string.no_file_chosen)
+        chosenFilename.text = if (uri == null || uri.toString() == "") {
+            noFileChosen
+        } else if (!uri.validFilePath(this)) {
+            // Fallback on the last display name if the path isn't valid.
+            val prefs = getSharedPreferences(packageName, MODE_PRIVATE)
+            prefs.getString(CHOSEN_FILE_NAME_KEY, "") ?: noFileChosen
+        } else {
+            uri.getDisplayName(this) ?: noFileChosen
+        }
+
+        // Save the file uri.
+        fileToRead = uri
+    }
+
+    private fun verifyStoragePermissions() {
+        // Check if we have write permission.
+        if (Build.VERSION.SDK_INT >= 23) {
+            val permission = this.checkSelfPermission(WRITE_EXTERNAL_STORAGE)
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // We don't have permission so prompt the user
+                requestPermissions(
+                        PERMISSIONS_STORAGE,
+                        REQUEST_EXTERNAL_STORAGE
+                )
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super<FileChooser>.onActivityResult(requestCode, resultCode, data)
+        super<MyAppCompatActivity>.onActivityResult(requestCode, resultCode, data)
+    }
+
+    companion object {
+        // Storage Permissions
+        private const val REQUEST_EXTERNAL_STORAGE = 1
+        private val PERMISSIONS_STORAGE = arrayOf(
+                READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE
+        )
+    }
+}
--- app/src/main/java/com/danefinlay/ttsutil/FileUriHelper.kt
+++ app/src/main/java/com/danefinlay/ttsutil/FileUriHelper.kt
@@ -3,136 +3,58 @@ package com.danefinlay.ttsutil
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import java.io.InputStream
 
-private const val CHOSEN_FILE_PREF_KEY = "$APP_NAME.CHOSEN_FILE_PREF_KEY"
-const val CHOSEN_FILE_LAST_MODIFIED_PREF_KEY =
-        "$APP_NAME.CHOSEN_FILE_LAST_MODIFIED_PREF_KEY"
 
-var Context.chosenFileUri: Uri?
-    get() {
-        // Get chosenFilePath using shared preferences if possible
-        val prefs = getSharedPreferences(packageName, Context.MODE_PRIVATE)
-        val result = if ( prefs.contains(CHOSEN_FILE_PREF_KEY) ) {
-            Uri.parse(prefs.getString(CHOSEN_FILE_PREF_KEY, ""))
-        } else {
-            null
-        }
-
-        return result
-    }
-    set(value) {
-        // Update shared preferences asynchronously every time this is updated
-        val prefs = getSharedPreferences(packageName, Context.MODE_PRIVATE)
-        prefs.edit().putString(CHOSEN_FILE_PREF_KEY, value.toString()).apply()
-    }
-
-val Context.fileUriProperties: Map<String, String>?
-    get() {
-        val fileUri = setupFileUriForProcessing() ?: return null
-        return contentResolver.query(fileUri, null, null, null, null)?.use { cursor ->
-            val result = mutableMapOf<String, String>()
-            while ( cursor.moveToNext() ) {
-                cursor.columnNames.forEach {
-                    result[it] = cursor.getString(cursor.getColumnIndex(it))
-                }
-            }
-            result
-        }
-    }
-
-val Context.fileUriLastModified: Long?
-    get() {
-        val properties = fileUriProperties ?: return null
-        val lastModifiedKey = "last_modified"
-        return try {
-            properties[lastModifiedKey]?.toLong()
-        } catch (e: NumberFormatException) { null }
-    }
-
-var Context.fileUriLastModifiedPrefValue: Long?
-    get() {
-        val prefs = getSharedPreferences(packageName, Context.MODE_PRIVATE)
-        val result = if ( prefs.contains(CHOSEN_FILE_LAST_MODIFIED_PREF_KEY) ) {
-            try {
-            prefs.getString(CHOSEN_FILE_LAST_MODIFIED_PREF_KEY, null)?.toLong()
-            } catch (e: NumberFormatException) { null }
-        } else null
-        return result
-    }
-    set(value) {
-        val prefs = getSharedPreferences(packageName, Context.MODE_PRIVATE)
-        prefs.edit().putString(
-                CHOSEN_FILE_LAST_MODIFIED_PREF_KEY,
-                value.toString()
-        ).apply()
-    }
-
-/**
- * Handles FileUri exceptions and returns whether the block threw an exception as well as
- * the result from the `block` lambda.
- */
-inline fun <R> Context.handleFileUriExceptions(block: () -> R): Pair<Boolean, R?> {
-    return try {
-        val result = block()
-        Pair(false, result)
-    } catch (e: Exception) {
-        when (e) {
-            is NullPointerException, is SecurityException -> {
-                // SecurityException: the file probably doesn't exist or is inaccessible.
-                // NullPointerException: Cursor was null (probably)
-                chosenFileUri = null
-
-                Pair(true, null)
-            }
-
-            else -> throw e // Re-throw others; we shouldn't catch everything...
-        }
-    }
-}
-
-val Context.chosenPathIsValid: Boolean
-    get() {
-        val (exceptional, result) = handleFileUriExceptions {
-            val fileUri = setupFileUriForProcessing() ?: return false
-
-            // Check that the returned Cursor from the content resolver query is not null
-            // If it is then the file has been moved, deleted, or is inaccessible.
-            val cursor = contentResolver.query(fileUri, null, null, null, null) ?: return false
-
-            cursor.close()
-
-            true
-        }
-
-        return !exceptional && result == true
-    }
-
-fun Context.setupFileUriForProcessing(): Uri? {
-    val fileUri = chosenFileUri ?: return null
-    val contentResolver = contentResolver
+fun Uri.setupUriForProcessing(ctx: Context): Uri {
+    val contentResolver = ctx.contentResolver
     val takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION or
             Intent.FLAG_GRANT_WRITE_URI_PERMISSION
 
     // Handle permissions properly.
-    contentResolver.takePersistableUriPermission(fileUri, takeFlags)
-
-    return fileUri
+    contentResolver.takePersistableUriPermission(this, takeFlags)
+    return this
 }
 
-val Context.uriContent: List<String>?
-    get() {
-        val fileUri = setupFileUriForProcessing() ?: return null
-        val inputStream = contentResolver.openInputStream(fileUri)
-        val reader = inputStream.bufferedReader()
-        val lines = reader.lineSequence().toList()
 
-        reader.close()
-        inputStream.close()
-
-        return lines
+fun Uri.validFilePath(ctx: Context): Boolean {
+    // Check that the returned Cursor from the content resolver query is not null
+    // If it is then the file has been moved, deleted, or is inaccessible.
+    return try {
+        setupUriForProcessing(ctx)
+        val cursor = ctx.contentResolver.query(this, null, null,
+                null, null)
+        cursor?.close()
+        /* return */ cursor != null
+    } catch (e: SecurityException) {
+        // The file probably doesn't exist or is inaccessible.
+        false
     }
+}
 
-val Context.uriAbsFilePath: String?
-    get() {
-        TODO()
+
+fun Uri.getFileProperties(ctx: Context): Map<String, String>? {
+    return ctx.contentResolver.query(this, null, null, null,
+            null)?.use { cursor ->
+        val result = mutableMapOf<String, String>()
+        while ( cursor.moveToNext() ) {
+            cursor.columnNames.forEach {
+                result[it] = cursor.getString(cursor.getColumnIndex(it))
+            }
+        }
+        result
     }
+}
+
+
+fun Uri.getDisplayName(ctx: Context): String? {
+    // Return the display name property, falling back on the path if the
+    // display name is not available.
+    return getFileProperties(ctx)?.get("_display_name") ?: path
+}
+
+
+fun Uri.getContent(ctx: Context): InputStream? {
+    setupUriForProcessing(ctx)
+    return ctx.contentResolver.openInputStream(this)
+}
