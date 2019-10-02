3d5d3514f56bf1c11275b88d05dd0d501151fe28
Dane Finlay <Danesprite@posteo.net>
2019-10-02 17:00:44 +1000

    Move a few things from MainActivity to SpeakerActivity

--- app/src/main/java/com/danefinlay/ttsutil/MainActivity.kt
+++ app/src/main/java/com/danefinlay/ttsutil/MainActivity.kt
@@ -4,12 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.speech.tts.TextToSpeech.Engine.ACTION_GET_SAMPLE_TEXT
-import android.speech.tts.TextToSpeech.Engine.EXTRA_SAMPLE_TEXT
 import android.support.design.widget.TextInputLayout
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import org.jetbrains.anko.AlertDialogBuilder
 import org.jetbrains.anko.find
 import org.jetbrains.anko.onClick
 import org.jetbrains.anko.toast
@@ -34,11 +32,6 @@ class MainActivity : SpeakerActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if ( savedInstanceState == null ) {
-            // Check if there is a TTS engine is installed on the device.
-            checkTTS(CHECK_TTS)
-        }
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
@@ -143,10 +136,13 @@ class MainActivity : SpeakerActivity() {
         }
     }
 
-    private fun checkTTS(code: Int) {
-        val check = Intent()
-        check.action = TextToSpeech.Engine.ACTION_CHECK_TTS_DATA
-        startActivityForResult(check, code)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CHECK_TTS_SPEAK_AFTERWARDS &&
+                resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+            // The Speaker should have been initialised by the super method.
+            speakFromInputLayout()
+        }
     }
 
     private fun speakFromInputLayout() {
@@ -155,70 +151,10 @@ class MainActivity : SpeakerActivity() {
             speaker?.speak(listOf(content))
         } else {
             // Get sample text from the TTS engine.
+            // This is handled by SpeakerActivity.onActivityResult().
             val intent = Intent()
             intent.action = ACTION_GET_SAMPLE_TEXT
             startActivityForResult(intent, SPEAK_SAMPLE_TEXT)
         }
-    }
-
-    private fun showNoTTSDataDialog() {
-        AlertDialogBuilder(this).apply {
-            title(R.string.no_tts_data_alert_title)
-            message(R.string.no_tts_data_alert_message)
-            positiveButton(R.string.alert_positive_message) {
-                val install = Intent()
-                install.action = TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA
-                startActivityForResult(install, INSTALL_TTS_DATA)
-            }
-            negativeButton(R.string.alert_negative_message1)
-            show()
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when (requestCode) {
-            CHECK_TTS, CHECK_TTS_SPEAK_AFTERWARDS -> {
-                if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
-                    // Start the speaker.
-                    myApplication.startSpeaker()
-                    if (requestCode == CHECK_TTS_SPEAK_AFTERWARDS) {
-                        speakFromInputLayout()
-                    }
-                } else {
-                    // Show a dialogue *and then* start an activity to install a
-                    // text to speech engine if the user agrees.
-                    showNoTTSDataDialog()
-                }
-            }
-
-            INSTALL_TTS_DATA -> {
-                if (resultCode == TextToSpeech.ERROR) {
-                    // Show a dialog if installing TTS data failed.
-                    AlertDialogBuilder(this).apply {
-                        title(R.string.failed_to_get_tts_data_title)
-                        message(R.string.failed_to_get_tts_data_msg)
-                        positiveButton(R.string.alert_positive_message) {}
-                        show()
-                    }
-                }
-            }
-
-            SPEAK_SAMPLE_TEXT -> {
-                // Speak sample text
-                val sampleText = data?.getStringExtra(EXTRA_SAMPLE_TEXT) ?:
-                        ttsInputLayout.editText?.hint?.toString()
-                if (sampleText != null) {
-                    myApplication.speaker?.speak(sampleText)
-                }
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    companion object {
-        private const val CHECK_TTS = 1
-        private const val CHECK_TTS_SPEAK_AFTERWARDS = 2
-        private const val INSTALL_TTS_DATA = 3
-        private const val SPEAK_SAMPLE_TEXT = 4
     }
 }
--- app/src/main/java/com/danefinlay/ttsutil/SpeakerActivity.kt
+++ app/src/main/java/com/danefinlay/ttsutil/SpeakerActivity.kt
@@ -1,7 +1,11 @@
 package com.danefinlay.ttsutil
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.AlertDialogBuilder
 
 /**
  * Custom activity class so things like 'myApplication' and 'speaker' don't need to
@@ -14,4 +18,89 @@ open class SpeakerActivity: AppCompatActivity() {
 
     val speaker: Speaker?
         get() = myApplication.speaker
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if ( savedInstanceState == null ) {
+            // Check if there is a TTS engine is installed on the device.
+            // Only necessary if the Speaker is null.
+            if (speaker == null) {
+                checkTTS(CHECK_TTS)
+            }
+        }
+    }
+
+    fun checkTTS(code: Int) {
+        val check = Intent()
+        check.action = TextToSpeech.Engine.ACTION_CHECK_TTS_DATA
+        startActivityForResult(check, code)
+    }
+
+    private fun showNoTTSDataDialog() {
+        AlertDialogBuilder(this).apply {
+            title(R.string.no_tts_data_alert_title)
+            message(R.string.no_tts_data_alert_message)
+            positiveButton(R.string.alert_positive_message) {
+                val install = Intent()
+                install.action = TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA
+                startActivityForResult(install, INSTALL_TTS_DATA)
+            }
+            negativeButton(R.string.alert_negative_message1)
+            show()
+        }
+    }
+
+    private fun showTTSInstallFailureDialog() {
+        // Show a dialog if installing TTS data failed.
+        AlertDialogBuilder(this).apply {
+            title(R.string.failed_to_get_tts_data_title)
+            message(R.string.failed_to_get_tts_data_msg)
+            positiveButton(R.string.alert_positive_message) {}
+            show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            CHECK_TTS, CHECK_TTS_SPEAK_AFTERWARDS -> {
+                if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                    // Start the speaker.
+                    myApplication.startSpeaker()
+                } else {
+                    // Show a dialogue *and then* start an activity to install a
+                    // text to speech engine if the user agrees.
+                    showNoTTSDataDialog()
+                }
+            }
+
+            INSTALL_TTS_DATA -> {
+                if (resultCode == TextToSpeech.ERROR) {
+                    showTTSInstallFailureDialog()
+                }
+            }
+
+            SPEAK_SAMPLE_TEXT -> {
+                // Get sample text
+                val extraKey = TextToSpeech.Engine.EXTRA_SAMPLE_TEXT
+                var sampleText = data?.getStringExtra(extraKey)
+                if (sampleText.isNullOrBlank()) {
+                    sampleText = getString(R.string.sample_tts_sentence)
+                }
+
+                // Get the Speaker to read it if possible.
+                if (speaker.isReady()) {
+                    speaker?.speak(sampleText)
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    companion object {
+        const val CHECK_TTS = 1
+        const val CHECK_TTS_SPEAK_AFTERWARDS = 2
+        const val INSTALL_TTS_DATA = 3
+        const val SPEAK_SAMPLE_TEXT = 4
+    }
 }
