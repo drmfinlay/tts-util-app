1de84c48a3241f2b57538cfa86d5b5345c4c13c1
Dane Finlay <Danesprite@posteo.net>
2019-10-11 14:43:33 +1100

    Implement Read Clipboard action in QuickShareActivity
    This makes the "Read clipboard" launcher shortcut work.

--- app/src/main/java/com/danefinlay/ttsutil/QuickShareActivity.kt
+++ app/src/main/java/com/danefinlay/ttsutil/QuickShareActivity.kt
@@ -2,29 +2,47 @@ package com.danefinlay.ttsutil
 
 import android.content.Intent
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 
 /**
  * Activity to quickly read text from an input source.
+ * Reading text is done via SpeakerIntentService.
  */
 class QuickShareActivity : SpeakerActivity() {
 
-    private var waitForActivityResult = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (savedInstanceState == null && !speaker.isReady()) {
-            checkTTS(CHECK_TTS)
-            waitForActivityResult = true
-        }
+        if (speaker.isReady()) {
+            // Start the appropriate service action if the Speaker is ready.
+            startServiceAction()
 
-        // Finish the activity.
-        if (!waitForActivityResult) finish()
+            // Finish the activity.
+            finish()
+        } else {
+            // Check (and eventually setup) text-to-speech.
+            checkTTS(CHECK_TTS_SPEAK_AFTERWARDS)
+        }
+    }
+
+    private fun startServiceAction() {
+        val intent = intent ?: return
+        when (intent.action) {
+            ACTION_READ_CLIPBOARD -> {
+                SpeakerIntentService.startActionReadClipboard(this)
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // Finish the activity.
-        finish()
+        // Start the appropriate service action now that the Speaker is ready.
+        if (requestCode == CHECK_TTS_SPEAK_AFTERWARDS &&
+                resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+            startServiceAction()
+
+            // Finish the activity.
+            finish()
+        }
     }
 }
