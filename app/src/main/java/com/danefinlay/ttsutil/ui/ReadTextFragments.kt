91c0f7e619511d48e5f11484bc22e6831933fbba
Dane Finlay <dane@danefinlay.net>
2021-11-29 15:23:19 +1100

    Simplify source code file ReadTextFragments.kt

--- app/src/main/java/com/danefinlay/ttsutil/ui/ReadTextFragments.kt
+++ app/src/main/java/com/danefinlay/ttsutil/ui/ReadTextFragments.kt
@@ -20,7 +20,6 @@
 
 package com.danefinlay.ttsutil.ui
 
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
@@ -38,24 +37,32 @@ import org.jetbrains.anko.support.v4.find
 
 abstract class ReadTextFragmentBase : Fragment() {
 
-    private val speakButton: ImageButton
-        get() = find(R.id.speak_button)
-
-    private val stopSpeakingButton: ImageButton
-        get() = find(R.id.stop_speaking_button)
-
-    protected val inputLayout: TextInputLayout
+    private val inputLayout: TextInputLayout
         get() = find(R.id.input_layout)
 
-    protected val myActivity: SpeakerActivity
-        get() = (activity as SpeakerActivity)
+    var inputLayoutContent: String?
+        set(value) {
+            val text = value ?: ""
+            inputLayout.editText?.text?.apply {
+                clear()
+                append(text)
+            }
+        }
+        get() {
+            return inputLayout.editText?.text?.toString()
+        }
+
+    private val myActivity: SpeakerActivity
+        get() = activity as SpeakerActivity
 
     private val speaker: Speaker?
         get() = myActivity.speaker
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        speakButton.onClick {
+
+        // Set OnClick listener for the start/stop speaking buttons.
+        find<ImageButton>(R.id.speak_button).onClick {
             if (speaker.isReady()) {
                 speakFromInputLayout()
             } else {
@@ -63,8 +70,7 @@ abstract class ReadTextFragmentBase : Fragment() {
                 myActivity.showSpeakerNotReadyMessage()
             }
         }
-
-        stopSpeakingButton.onClick {
+        find<ImageButton>(R.id.stop_speaking_button).onClick {
             speaker?.stopSpeech()
         }
     }
@@ -74,12 +80,7 @@ abstract class ReadTextFragmentBase : Fragment() {
 
         // Restore fragment instance state here.
         val content = savedInstanceState?.getString("inputLayoutContent")
-        if (content != null) {
-            inputLayout.editText?.text?.apply {
-                clear()
-                append(content)
-            }
-        }
+        if (content != null) inputLayoutContent = content
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -87,15 +88,14 @@ abstract class ReadTextFragmentBase : Fragment() {
 
         // Save fragment instance state here.
         if (view != null) {
-            val content = inputLayout.editText?.text?.toString()
-            outState.putString("inputLayoutContent", content)
+            outState.putString("inputLayoutContent", inputLayoutContent)
         }
     }
 
     private fun speakFromInputLayout() {
-        val content = inputLayout.editText?.text?.toString()
-        if (content.isNullOrBlank()) {
-            myActivity.toast(R.string.cannot_speak_empty_text_msg)
+        val content = inputLayoutContent ?: ""
+        if (content.isBlank()) {
+            context?.toast(R.string.cannot_speak_empty_text_msg)
             speaker?.speak(inputLayout.hint?.toString())
             return
         }
@@ -107,31 +107,12 @@ abstract class ReadTextFragmentBase : Fragment() {
 
 class ReadTextFragment : ReadTextFragmentBase() {
 
-    val intent: Intent?
-        get() = myActivity.intent
-
-    private val clearBoxButton: ImageButton
-        get() = find(R.id.clear_box_button)
-
-    private val mem1: ImageButton
-        get() = find(R.id.Memory1)
-
-    private val mem2: ImageButton
-        get() = find(R.id.Memory2)
-
-    private val mem3: ImageButton
-        get() = find(R.id.Memory3)
-
-    private val mem4: ImageButton
-        get() = find(R.id.Memory4)
-
     /**
      * Event listener for the memory buttons.
      */
     private class MemoryButtonEventListener(val memoryKey: String,
                                             val prefs: SharedPreferences,
-                                            val inputLayout: TextInputLayout,
-                                            val ctx: Context?
+                                            val fragment: ReadTextFragmentBase
     ) : View.OnClickListener, View.OnLongClickListener {
 
         override fun onClick(v: View?) {
@@ -139,25 +120,22 @@ class ReadTextFragment : ReadTextFragmentBase() {
             // display a message.
             val text = prefs.getString(memoryKey, "")
             if (text.isNullOrEmpty()) {
-                ctx?.toast(R.string.mem_slot_empty_msg)
+                fragment.context?.toast(R.string.mem_slot_empty_msg)
             } else {
-                inputLayout.editText?.text?.apply {
-                    clear()
-                    append(text)
-                }
+                fragment.inputLayoutContent = text
             }
         }
 
         override fun onLongClick(v: View?): Boolean {
             // Store the text field content in memory, displaying an appropriate
             // message.
-            val content = inputLayout.editText?.text?.toString()
-            val messageId = if (content.isNullOrEmpty()) R.string.mem_slot_cleared_msg
+            val content = fragment.inputLayoutContent ?: ""
+            val messageId = if (content.isEmpty()) R.string.mem_slot_cleared_msg
                             else R.string.mem_slot_set_msg
             val editor: SharedPreferences.Editor = prefs.edit()
             editor.putString(memoryKey, content)
             editor.apply()
-            ctx?.toast(messageId)
+            fragment.context?.toast(messageId)
             return true
         }
     }
@@ -173,38 +151,34 @@ class ReadTextFragment : ReadTextFragmentBase() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        clearBoxButton.onClick {
-            inputLayout.editText?.text?.clear()
+
+        // Set OnClick listener for the clear box button.
+        find<ImageButton>(R.id.clear_box_button).onClick {
+            inputLayoutContent = ""
         }
 
         // Set OnClick and OnLongClick event listeners for each memory button.
         val ctx = context /* Activity context */
         val prefs = PreferenceManager.getDefaultSharedPreferences(ctx)
-        listOf(mem1, mem2, mem3, mem4).forEachIndexed { i, button ->
-            val memoryKey = "mem${i + 1}"  // mem1..mem4
-            val listener = MemoryButtonEventListener(memoryKey, prefs, inputLayout,
-                                                     ctx)
-            button.setOnClickListener(listener)
-            button.setOnLongClickListener(listener)
+        listOf(R.id.Memory1, R.id.Memory2, R.id.Memory3, R.id.Memory4)
+                .forEachIndexed { i, id ->
+                    val button = find<ImageButton>(id)
+                    val memoryKey = "mem${i + 1}"  // mem1..mem4
+                    val listener = MemoryButtonEventListener(memoryKey, prefs,
+                            this)
+                    button.setOnClickListener(listener)
+                    button.setOnLongClickListener(listener)
         }
 
         // Handle ACTION_SEND.
+        val intent = activity?.intent
         if (savedInstanceState == null && intent?.action == Intent.ACTION_SEND) {
-            val text = intent?.getStringExtra(Intent.EXTRA_TEXT)
-            if (text != null) {
-                inputLayout.editText?.text?.apply {
-                    clear()
-                    append(text)
-                }
-            }
+            inputLayoutContent = intent.getStringExtra(Intent.EXTRA_TEXT)
         }
     }
 }
 
 class ReadClipboardFragment : ReadTextFragmentBase() {
-
-    private val updateTextFieldButton: ImageButton
-        get() = find(R.id.update_text_field_button)
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -217,16 +191,28 @@ class ReadClipboardFragment : ReadTextFragmentBase() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        updateTextFieldButton.onClick {
-            onClickUpdateTextFieldButton()
+
+        // Set OnClick listener for the update button.
+        find<ImageButton>(R.id.update_text_field_button).onClick {
+            // Get the current clipboard text.
+            val text = context?.getClipboardText() ?: ""
+
+            // Update the text field.
+            inputLayoutContent = text
+
+            // Display a message if the clipboard was empty.
+            if (text.isEmpty()) {
+                activity?.toast(R.string.clipboard_is_empty_blank_msg)
+            }
         }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        if (savedInstanceState == null) {
-            val text = myActivity.getClipboardText()
+        val ctx = context
+        if (savedInstanceState == null && ctx != null) {
+            val text = ctx.getClipboardText()
 
             // Android 10 restricts access to the clipboard for privacy reasons.
             // Accessing it from the foreground activity is permitted, but it seems
@@ -234,35 +220,15 @@ class ReadClipboardFragment : ReadTextFragmentBase() {
             if (text == null && Build.VERSION.SDK_INT >= 29) {
                 doAsync {
                     Thread.sleep(100)
-                    myActivity.runOnUiThread {
+                    ctx.runOnUiThread {
                         if (this@ReadClipboardFragment.view != null)
-                            updateTextField(myActivity.getClipboardText())
+                            inputLayoutContent = ctx.getClipboardText()
                     }
                 }
             } else {
                 // Otherwise just update the text field.
-                updateTextField(text)
+                inputLayoutContent = text
             }
-        }
-    }
-
-    private fun updateTextField(text: String?) {
-        inputLayout.editText?.text?.clear()
-        if (text != null) {
-            inputLayout.editText?.text?.append(text)
-        }
-    }
-
-    private fun onClickUpdateTextFieldButton() {
-        // Get the current clipboard text.
-        val text = myActivity.getClipboardText()
-
-        // Update the text field.
-        updateTextField(text)
-
-        // Display a message if the clipboard was empty.
-        if (text.isNullOrEmpty()) {
-            myActivity.toast(R.string.clipboard_is_empty_blank_msg)
         }
     }
 }
